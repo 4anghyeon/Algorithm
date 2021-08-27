2c2df40760a25ab46d6f2a3dc5f83f6bf6fcf8c4
sanghyeon lee <uphyeon@kakao.com>
2021-08-27 21:45:16 +0900

    test

--- 프로그래머스/java/전화번호 목록/Solution.java
+++ 프로그래머스/java/전화번호 목록/Solution.java
@@ -1,5 +1,18 @@
+import java.util.*;
+import java.util.stream.*;
+
 public class Solution {
+
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        solution(new String[]{"test"});
+    }
+
+    public static void solution(String[] phone_book) {
+        HashSet<String> hashSet = (HashSet<String>) Arrays.stream(phone_book).collect(Collectors.toSet());
+
+        for (String key : hashSet)
+            for (int j = 1; j <= key.length() - 1; j++) {
+                if (hashSet.contains(key.substring(0, j))) return;
+            }
     }
 }
