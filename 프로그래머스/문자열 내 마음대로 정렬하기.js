28edaf23e73731b78a3940555581126675d55126
sanghyeon lee <uphyeon@kakao.com>
2022-08-04 00:21:22 +0900

    commited

--- 프로그래머스/문자열 내 마음대로 정렬하기.js
+++ 프로그래머스/문자열 내 마음대로 정렬하기.js
@@ -1,13 +1,8 @@
 function solution(strings, n) {
-    var answer = [];
-    var arr = [];
-    strings.forEach((string) => {
-        arr.push([string, string[n].charCodeAt()]);
-    });
-    arr.sort((a, b) => {
-        if (a[1] === b[1]) return a > b ? 1 : -1;
-        return a[1] - b[1];
+    strings.sort((a, b) => {
+        if (a[n].charCodeAt() === b[n].charCodeAt()) return a > b ? 1 : -1;
+        return a[n].charCodeAt() - b[n].charCodeAt();
     });
 
-    return arr.map((a) => a[0]);
+    return strings;
 }
