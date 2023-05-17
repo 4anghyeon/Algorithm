3459d69cfb48a50ae1bc44c7fcd86837d54304b9
sanghyeon lee <uphyeon@kakao.com>
2023-05-17 15:23:18 +0900

    add

---
+++ 백준/11659. 구간합 구하기/Main.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("test");
+
+        int numberOfData = sc.nextInt();
+        // int numberOfQuery = sc.nextInt();
+        // int[] sumArray = new int[numberOfData];
+
+        // for (int i = 0; i < numberOfData; i++) {
+        // if (i == 0)
+        // sumArray[i] = sc.nextInt();
+        // else {
+        // sumArray[i] = sumArray[i - 1] + sc.nextInt();
+        // }
+        // }
+
+        // for (int i = 0; i < numberOfQuery; i++) {
+        // int start = sc.nextInt();
+        // int end = sc.nextInt();
+        // if (end > start)
+        // System.out.println(sumArray[end] - sumArray[start - 1]);
+        // else
+        // System.out.println(sumArray[start] - sumArray[end - 1]);
+        // }
+
+    }
+}
