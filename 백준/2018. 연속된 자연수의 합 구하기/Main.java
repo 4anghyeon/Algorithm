bc942c24183824c99ada1fc37ca2b23fe5899073
uphyeon@kakao.com <uphyeon@kakao.com>
2023-05-22 23:15:51 +0900

    added

---
+++ 백준/2018. 연속된 자연수의 합 구하기/Main.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int count = 1;
+        int startIndex = 1;
+        int endIndex = 1;
+        int sum = 1;
+
+        while (endIndex != n) {
+            if (sum == n) {
+                endIndex++;
+                count++;
+                sum += endIndex;
+            } else if (sum > n) {
+                sum -= startIndex;
+                startIndex++;
+            } else {
+                endIndex++;
+                sum += endIndex;
+            }
+        }
+
+        System.out.println(count);
+
+    }
+}
