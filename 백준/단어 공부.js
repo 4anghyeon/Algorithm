d511b819bbfc11878f8be177631917f19850ef28
sanghyeon <uphyeon@kakao.com>
2023-10-24 13:02:16 +0900

    백준 단어 공부

--- 백준/단어 공부.js
+++ 백준/단어 공부.js
@@ -1,6 +1,7 @@
 let fs = require('fs');
 let input = fs.readFileSync('/dev/stdin').toString().trim();
 
+// 1번째 풀이
 let map = {};
 
 [...input].forEach(item => {
@@ -17,3 +18,22 @@ const maxValue = Math.max(...values);
 
 if (values.filter(v => v === maxValue).length > 1) console.log("?")
 else console.log(Object.keys(map)[values.findIndex(v => v === maxValue)]);
+
+
+// 2번째 풀이
+let arr = [];
+
+[...input].forEach(item => {
+  const char = item.toUpperCase().charCodeAt();
+  if (!arr[char]) {
+    arr[char] = 1;
+  } else {
+    arr[char] += 1;
+  }
+});
+
+const filtered = arr.filter(i => i);
+const maxValue = Math.max(...filtered);
+
+if (arr.filter(v => v === maxValue).length > 1) console.log("?")
+else console.log(String.fromCharCode(arr.findIndex(v => v === maxValue)))
