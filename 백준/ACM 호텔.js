70293d97f9adf9c8eec06b7e5bfebd188e6fb891
sanghyeon <uphyeon@kakao.com>
2023-10-25 10:27:28 +0900

    백준 ACM 호텔

--- 백준/ACM 호텔.js
+++ 백준/ACM 호텔.js
@@ -3,7 +3,7 @@ let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 
 const testLength = input[0];
 for (let i = 1; i <= testLength; i++) {
-  const [height, width, n] = input[i].split(" ");
+  const [height, , n] = input[i].split(" ");
   const floor = n % height === 0 ? height : n % height;
   console.log(`${floor}${(Math.ceil(n / height) + "").padStart(2, "0")}`)
 }
