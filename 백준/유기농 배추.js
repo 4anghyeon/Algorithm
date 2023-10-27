95be1c8860001336ec4ac1a4474dfe801a666de9
sanghyeon <uphyeon@kakao.com>
2023-10-27 11:48:16 +0900

    ver3

--- 백준/유기농 배추.js
+++ 백준/유기농 배추.js
@@ -1,54 +1,32 @@
-const input =("10\n" +
-  "3 2 4\n" +
-  "1 0\n" +
-  "0 1\n" +
-  "1 1\n" +
-  "2 1\n" +
-  "3 3 6\n" +
-  "2 0\n" +
-  "0 1\n" +
-  "2 1\n" +
-  "0 2\n" +
-  "1 2\n" +
-  "2 2\n" +
-  "2 2 2\n" +
-  "0 1\n" +
-  "1 0\n" +
-  "2 2 2\n" +
-  "0 1\n" +
-  "1 0\n" +
-  "2 3 5\n" +
-  "0 0\n" +
-  "0 1\n" +
-  "0 2\n" +
-  "1 0\n" +
-  "1 1\n" +
-  "2 2 2\n" +
-  "0 1\n" +
-  "1 0\n" +
-  "2 2 3\n" +
+const input =("2\n" +
+  "10 8 17\n" +
   "0 0\n" +
   "1 0\n" +
-  "0 1\n" +
-  "3 2 5\n" +
-  "0 0\n" +
-  "0 1\n" +
   "1 1\n" +
-  "2 1\n" +
-  "2 0\n" +
-  "2 2 3\n" +
-  "0 0\n" +
-  "1 1\n" +
-  "0 1\n" +
-  "50 50 1\n" +
-  "49 49\n" +
-  "50 50 1\n" +
-  "0 0").split("\n");
-
-console.time("test");
+  "4 2\n" +
+  "4 3\n" +
+  "4 5\n" +
+  "2 4\n" +
+  "3 4\n" +
+  "7 4\n" +
+  "8 4\n" +
+  "9 4\n" +
+  "7 5\n" +
+  "8 5\n" +
+  "9 5\n" +
+  "7 6\n" +
+  "8 6\n" +
+  "9 6\n" +
+  "10 10 1\n" +
+  "5 5").split("\n");
 
 const testCaseSize = +input[0];
 let testStartIndex = 1;
+
+const dx = [-1, 1, 0, 0];
+const dy = [0, 0, -1, 1];
+
+let answer = "";
 
 for (let i = 0; i < testCaseSize; i++) {
   const [width, height, cabbageAmount] = input[testStartIndex].split(" ").map(Number);
@@ -67,40 +45,35 @@ for (let i = 0; i < testCaseSize; i++) {
     field[+x][+y] = 1;
   });
 
-  const visitedNodes = new Set();
   const bfs = (field, node) => {
     const queue = [node];
-    const movement = [1, -1];
 
-    let count  = 0;
-
+    // 인접 노드를 다 돌때까지 반복
     while (queue.length > 0) {
       let [x, y] = queue.shift();
-      visitedNodes.add(`${x}${y}`)
 
-      // 좌우에 인접노드가 있나 검색
-      for (let i = 0; i < movement.length; i++) {
-        const nx = x + movement[i];
-        if (nx < width && nx >= 0 && !visitedNodes.has(`${nx}${y}`) && field[nx][y] === 1) {
-          queue.push([nx, y])
+      if (field[x][y] === 0) continue; // 배추가 없는 곳은 그냥 지나친다
+
+      field[x][y] = 0; // 배추가 있다고 체크한 곳은 0으로 변환!
+
+      // 상하좌우에 인접노드가 있나 검색
+      for (let i = 0; i < 4; i++) {
+        const nx = x + dx[i];
+        const ny = y + dy[i];
+        if (nx < width && nx >= 0 && ny < height && ny >= 0) {
+          if (field[nx][ny] === 1) {
+            // 돌아볼 인접 노드 목록에 추가
+            queue.push([nx, ny])
+          }
         }
       }
-
-      // 상하에 인접노드가 있나 검색
-      for (let i = 0; i < movement.length; i++) {
-        const ny = y + movement[i];
-        if (ny < height && ny >= 0 && !visitedNodes.has(`${x}${ny}`) && field[x][ny] === 1) {
-          queue.push([x, ny])
-        }
-      }
-      count++;
     }
   }
 
   let count = 0;
   for (let i = 0; i < width; i++) {
     for (let j = 0; j < height; j++) {
-      if (field[i][j] === 1 && !visitedNodes.has(`${i}${j}`)) {
+      if (field[i][j] === 1) {
         bfs(field, [i, j]);
         count++;
       }
@@ -108,7 +81,6 @@ for (let i = 0; i < testCaseSize; i++) {
   }
 
   testStartIndex = testStartIndex + cabbageAmount + 1;
-  console.log(count)
+  answer += count + "\n";
 }
-
-console.timeEnd("test")
+console.log(answer)
