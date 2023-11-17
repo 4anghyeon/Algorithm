04fb90d15ab0704fb6a4db3de27a42c0aa82ca86
sanghyeon <uphyeon@kakao.com>
2023-11-17 11:26:47 +0900

    [level 1] Title: 덧칠하기, Time: 2.63 ms, Memory: 38.7 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.js
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.js
@@ -1,12 +1,18 @@
 function solution(n, m, section) {
-    let nowPoint = 0;
-    let answer = 0;
+    const painted = new Array(n).fill(true);
+    section.forEach(s => painted[s-1] = false);
+    var answer = 0;
     
-    for (let i = 0; i < section.length; i++) {
-        if (nowPoint < section[i]) {
-            answer++;
-            nowPoint = section[i] + m - 1; 
-        } 
+    let diffArr = [];
+    for (let i = 0; i < section.length - 1; i++) {
+        diffArr.push(section[i + 1] - section[i] + 1)
     }
-    return answer;
+    
+    console.log(diffArr)
+    
+    let max = section[section.length - 1];
+    let min = section[0];
+    let diff = max - min;
+    if (diff < m) return 1;
+    return (max - min) / m + 1;
 }
