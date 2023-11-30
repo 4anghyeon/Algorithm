f3149c8ca453e572906653e439c77d6bfce69183
sanghyeon <uphyeon@kakao.com>
2023-11-30 09:23:28 +0900

    [level 2] Title: 피보나치 수, Time: 126.79 ms, Memory: 39.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.js
@@ -0,0 +1,14 @@
+function solution(n) {
+    var answer = 0;
+    
+    let a = BigInt(0);
+    let b = BigInt(1);
+    for (let i = 2; i <= n ; i++) {
+        let temp = b;
+        b = a + b
+        a = temp;
+    }
+    
+    
+    return b % BigInt(1234567);
+}
