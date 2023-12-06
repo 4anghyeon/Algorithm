d65f49892a8bc5a1889d93464a2a35a5314b1511
sanghyeon <uphyeon@kakao.com>
2023-12-06 09:10:43 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 0.48 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
@@ -1,4 +1,20 @@
 function solution(today, terms, privacies) {
     var answer = [];
+    const todayNum = dateToNum(today);
+    
+    privacies.forEach((p,i) => {
+        const [date, term] = p.split(" ");
+        const t = terms.find(t => t.includes(term)).split(" ");
+
+        if (todayNum - dateToNum(date) >= +t[1] * 28) {
+            answer.push(i + 1)
+        }
+    })
+    
     return answer;
+}
+
+const dateToNum = date => {
+    const dateArr = date.split(".");
+    return +dateArr[0] * 336 + (+dateArr[1] - 1) * 28 + (+dateArr[2] - 1);
 }
