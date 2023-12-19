80d8e953eeecbbb88027519b9457709101dae785
sanghyeon <uphyeon@kakao.com>
2023-12-19 09:56:23 +0900

    [level 2] Title: n^2 배열 자르기, Time: 5.88 ms, Memory: 44.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.js
@@ -0,0 +1,27 @@
+function solution(n, left, right) {   
+    // 1
+    // [ 1 ]
+    // 2
+    // [ 1, 2, 2, 2 ]
+    // 3
+    // 	[1, 2, 3, 2, 2, 3, 3, 3, 3]
+    // 4
+    // 	[ [ 1, 2, 3, 4 , 2, 2, 3, 4 , 3, 3, 3, 4 , 4, 4, 4, 4 ] ]
+    // 5
+    // [ 1, 2, 3, 4, 5, 2, 2, 3, 4, 5, 3, 3, 3, 4, 5, 4, 4, 4, 4, 5, 5, 5, 5, 5, 5]
+    
+    let arr = [];
+    
+    for (let i = left + 1; i <= right + 1; i++) {
+        if (i <= n) arr.push(i);
+        else {
+            if (i % n === 0) arr.push(n);
+            else {
+                let a = Math.ceil(i / n);
+                if (i % n > a) arr.push(i % n);
+                else arr.push(a)
+            }
+        }
+    }
+    return arr;
+}
