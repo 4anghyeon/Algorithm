f5040ef0d8c4f0bdfa0251d8c87dc3bc31a326cb
sanghyeon <uphyeon@kakao.com>
2023-12-22 09:21:13 +0900

    [level 2] Title: 프로세스, Time: 0.47 ms, Memory: 30.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/42587. 프로세스/프로세스.js
@@ -0,0 +1,25 @@
+function solution(priorities, location) {
+    var answer = 0;
+    let newPriorities = [];
+    priorities.forEach(function(prioritie, index) {
+        newPriorities.push(index+"_"+prioritie);
+    });
+    
+    while (newPriorities.length !== 0) {
+        let isLargerThanFirst = false;
+        for (let i = 0 ; i < newPriorities.length; i++) {
+            if (newPriorities[0].split("_")[1] < newPriorities[i].split("_")[1]) {
+                newPriorities.push(newPriorities[0]);
+                newPriorities.splice(0,1);
+                isLargerThanFirst = true;
+                break;
+            }
+        }
+        if (!isLargerThanFirst) {
+            answer++;
+            if (parseInt(newPriorities[0].split("_")[0]) === location) return answer;
+            newPriorities.splice(0,1);
+        }
+    }
+    return answer;
+}
